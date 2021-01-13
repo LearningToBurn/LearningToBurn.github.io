ff0eb4b24e1b6c16f072f8a206f45db6137ceae7
peterb <prbayne@gmail.com>
2021-01-14 11:40:42 +1300

    Wire up auth success with new structure

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,11 @@
+---
+---
 
-function authSuccess(token) {
+function onLogin(token) {
   if(typeof visiting !== 'undefined') {
+    visiting.LTB_API_KEY = `{{ site.learntrack.api_key }}`
     visiting.makeVisit(token);
     visiting.loadVisited(token, $("body"));
   }
 }
+googleAuthor.authSuccess = onLogin;
